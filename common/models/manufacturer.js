06deb69423b4ce564106434aeb0a4104087c3c60
Alex Arriaga <alexzander.arriaga@base22.com>
2018-07-17 18:47:02 -0500

    Agregar ejemplo de datasource con MySQL

--- common/models/manufacturer.js
+++ common/models/manufacturer.js
@@ -2,4 +2,7 @@
 
 module.exports = function(Manufacturer) {
 
+  Manufacturer.validatesUniquenessOf('name', {
+    message: 'Manufacturer\'s name is not unique',
+  });
 };
--- common/models/vehicle.js
+++ common/models/vehicle.js
@@ -2,4 +2,19 @@
 
 module.exports = function(Vehicle) {
 
+  function validatorAsyncForManufacturerid(error, next) {
+    const Manufacturer = Vehicle.app.models.Manufacturer;
+
+    Manufacturer.exists(this.manufacturerId,
+      function(errorExists, instanceExists) {
+        if (errorExists || !instanceExists) {
+          error();
+        }
+        next();
+      });
+  }
+
+  Vehicle.validateAsync('manufacturerId', validatorAsyncForManufacturerid, {
+    message: 'Manufacturer doesn\'t exist',
+  });
 };
